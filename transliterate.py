16177307e8cf13a6e02888b9152600894df721dd
a11ce <a11ce@protonmail.com>
2019-11-02 20:37:47 -0400

    Add unidecode for non-latin alphabets

--- transliterate.py
+++ transliterate.py
@@ -1,5 +1,7 @@
 import re
 import sys
+import unidecode
+
 
 def main():
     pairs = []
@@ -8,15 +10,18 @@ def main():
             line = line.strip()
             pair = line.split(': ')
             pairs.append(pair)
-            
+
     for line in sys.stdin:
         line.rstrip()
         print(transLit(line, pairs))
 
+
 def transLit(text, pairs):
     out = text.lower()
     for pair in pairs:
-        out= re.sub(pair[0], pair[1], out)
-    return out
+        out = re.sub(pair[0], pair[1], out)
+    return unidecode.unidecode(out)
+
+
 if __name__ == "__main__":
     main()
